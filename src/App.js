6bfce9ceca7f6af46a73e4e110ef6904bb8769d5
atw29 <atw29@bath.ac.uk>
2019-01-13 21:49:46 +0000

    No Idea

--- src/App.js
+++ src/App.js
@@ -2,24 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Alex Weeks</a>
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a className="nav-link" href="#info">Info</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <Nav />
+      </header>
+    );
+  }
+}
+
+class Section extends Component {
+  render() {
+    return (
+      <div className="section">
+
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Section />
       </div>
     );
   }
